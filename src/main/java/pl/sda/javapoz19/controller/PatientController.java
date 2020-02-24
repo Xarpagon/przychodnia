8a16f909f671f8027e559be1fce463333c40dd6e
filip52 <53393033+filip52@users.noreply.github.com>
2020-02-24 14:08:30 +0100

    PatientRepo szukanie pacjenta, dodawanie i usuwanie. Nie ma jeszcze testu do usuwania.

--- src/main/java/pl/sda/javapoz19/controller/PatientController.java
+++ src/main/java/pl/sda/javapoz19/controller/PatientController.java
@@ -1,5 +1,7 @@
 package pl.sda.javapoz19.controller;
 
+import org.apache.coyote.Response;
+import org.hibernate.sql.Delete;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import pl.sda.javapoz19.model.Patient;
 import pl.sda.javapoz19.service.PatientService;
 
+
+import javax.servlet.http.HttpServlet;
 import java.util.List;
 
 @RestController
@@ -20,8 +24,19 @@ public class PatientController {
     public List<Patient> patientsByPeselOrLastName(@PathVariable String pesel, @PathVariable String lastName){
         return patientService.showAllPatientsByPeselOrLastName(pesel,lastName);
     }
+
     @PostMapping(value = "/patients/add", consumes = "application/json")
     public ResponseEntity<Long> submitPatient(@RequestBody Patient patient){
         return new ResponseEntity<>(patientService.addPatient(patient), HttpStatus.CREATED);
     }
+
+
+    @PostMapping(value ="/patients/delete/{id}", consumes = "application/json")
+    public ResponseEntity<Long>deletePatientsById(@PathVariable Long id){
+        patientService.deletePatientById(id);
+       return new ResponseEntity<Long>(HttpStatus.OK);
+
+    }
+
+
 }
--- src/main/java/pl/sda/javapoz19/repository/PatientRepository.java
+++ src/main/java/pl/sda/javapoz19/repository/PatientRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface PatientRepository extends JpaRepository<Patient,Long> {
 
     List<Patient> findAllByPeselOrLastName(String pesel,String lastName);
+    List<Patient> deletePatientById(Long id);
 
 
 }
