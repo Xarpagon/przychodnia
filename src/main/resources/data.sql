1da8ce98c4f3ca7d90e13f90f4e48ef9d75fcd1a
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-02-25 10:53:07 +0100

    AppointmentService addAppointmentScheduleByDoctor()

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,12 +17,18 @@ insert INTO USER (username, password, role_id) values ('admin', '$2a$10$IPJizmA0
 
 Insert into PATIENT (id,pesel, first_Name, last_Name,street, house_number,postcode,city,country,phone_Number,email)
 values (nextval('doctor_Seq'),'9999999999', 'Kamil', 'Nowek', 'szamotulska','12', '62-090', 'Rokietnica','Poland','+48123456789','kamil.nowek@wp.pl');
+Insert into PATIENT (id,pesel, first_Name, last_Name,street, house_number,postcode,city,country,phone_Number,email) values (nextval('patient_Seq'),'9999999999', 'Kamil', 'Nowek', 'Szamotulska','12', '62-090', 'Rokietnica','Poland','+48123456789','kamil.nowek@wp.pl');
+Insert into PATIENT (id,pesel, first_Name, last_Name,street, house_number,postcode,city,country,phone_Number,email) values (nextval('patient_Seq'),'6099999999', 'Janek', 'Kosynski', 'Janowiecka','13', '88-090', 'Rogowo','Poland','+48123456100','janek.kosynski@wp.pl');
 
 Insert into APPOINTMENT (id, doctor_id, appointment_Date, appointment_Time) values (nextval('appointment_Seq'), '2', '2020-02-24', '15:00');
 Insert into APPOINTMENT (id, doctor_id, appointment_Date, appointment_Time) values (nextval('appointment_Seq'), '1', '2020-02-25', '15:30');
 Insert into APPOINTMENT (id, doctor_id, appointment_Date, appointment_Time) values (nextval('appointment_Seq'), '3', '2020-02-26', '14:00');
 Insert into APPOINTMENT (id, doctor_id, appointment_Date, appointment_Time) values (nextval('appointment_Seq'), '2', '2020-02-25', '13:00');
 Insert into APPOINTMENT (id, doctor_id, appointment_Date, appointment_Time) values (nextval('appointment_Seq'), '2', '2020-03-25', '14:00');
+
+Insert into APPOINTMENT (id, doctor_id, patient_id, appointment_Date, appointment_Time, description) values (nextval('appointment_Seq'), '1', '1', '2020-03-03', '17:00', 'fever');
+Insert into APPOINTMENT (id, doctor_id, patient_id, appointment_Date, appointment_Time, description) values (nextval('appointment_Seq'), '1', '2', '2020-03-04', '10:00', 'hangover');
+
 
 
 commit;
--- src/test/java/pl/sda/javapoz19/service/AppointmentServiceTest.java
+++ src/test/java/pl/sda/javapoz19/service/AppointmentServiceTest.java
@@ -4,12 +4,14 @@ package pl.sda.javapoz19.service;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pl.sda.javapoz19.model.Address;
 import pl.sda.javapoz19.model.Doctor;
 import pl.sda.javapoz19.model.AppointmentDto;
 import pl.sda.javapoz19.model.Specialization;
+import pl.sda.javapoz19.repository.DoctorRepository;
 
 import java.time.LocalDate;
 
@@ -19,29 +21,48 @@ import java.time.LocalTime;
 @ExtendWith(SpringExtension.class)
 class AppointmentServiceTest {
 
-    private AppointmentService appointmentService = new AppointmentService(null);
+    @Autowired
+    private AppointmentService appointmentService;
 
     @Test
-    public void convertToEntitiesListShouldReturnSize4ForWorkingHours14To16(){
+    public void convertToEntitiesListShouldReturnSize4ForWorkingHours14To16() {
 
         //given
-            Address address = new Address("Brzozowa", "1", "88700", "Piła", "Poland");
-            Doctor doctor = new Doctor(1L, "82121619936", "John", "Warki",
-                    Specialization.UROLOGIST, address, "+48500600540", "awar@wp.pl");
-            AppointmentDto AppointmentDto = new AppointmentDto(doctor, LocalDate.of(2020,02,24),
-                    LocalTime.of(14,0),LocalTime.of(16,0),30);
+        Address address = new Address("Brzozowa", "1", "88700", "Piła", "Poland");
+        Doctor doctor = new Doctor(1L, "82121619936", "John", "Warki",
+                Specialization.UROLOGIST, address, "+48500600540", "awar@wp.pl");
+        AppointmentDto appointmentDto = new AppointmentDto(1l, LocalDate.of(2020, 02, 24),
+                LocalTime.of(14, 0), LocalTime.of(16, 0), 30);
 
-            int expectedListSize = 4;
-            LocalTime expected = LocalTime.of(15,0);
+        int expectedListSize = 4;
+
 
         //when
-        LocalTime actual = appointmentService.convertToEntitiesList(AppointmentDto).get(2).getAppointmentTime();
 
-        int actualSize = appointmentService.convertToEntitiesList(AppointmentDto).size();
+        int actualListSize = appointmentService.convertToEntitiesList(appointmentDto).size();
 
         //then
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expectedListSize, actualListSize);
     }
 
+    @Test
+    public void convertToEntitiesListShouldReturnAppointmentTime1430ForAppointmentIndex5() {
 
+
+    //given
+    Address address = new Address("Brzozowa", "1", "88700", "Piła", "Poland");
+    Doctor doctor = new Doctor(1L, "82121619936", "John", "Warki",
+            Specialization.UROLOGIST, address, "+48500600540", "awar@wp.pl");
+    AppointmentDto appointmentDto = new AppointmentDto(1l, LocalDate.of(2020, 02, 24),
+            LocalTime.of(12, 0), LocalTime.of(15, 0), 30);
+
+    LocalTime expectedAppointmentTime = LocalTime.of(14, 30);
+
+    //when
+
+    LocalTime actualAppointmentTime = appointmentService.convertToEntitiesList(appointmentDto).get(5).getAppointmentTime();
+
+    //then
+        Assert.assertEquals(expectedAppointmentTime,actualAppointmentTime);
+    }
 }
