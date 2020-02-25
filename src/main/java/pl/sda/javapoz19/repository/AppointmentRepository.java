86b16e9abe7ff3c6251cb712414c6efaf56bf7a5
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-02-25 14:49:05 +0100

    reservedAppointmentsByDoctor

--- src/main/java/pl/sda/javapoz19/repository/AppointmentRepository.java
+++ src/main/java/pl/sda/javapoz19/repository/AppointmentRepository.java
@@ -2,14 +2,21 @@ package pl.sda.javapoz19.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.sda.javapoz19.model.Appointment;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
     List<Appointment> findAllByDoctorId(Long id);
+
+    List<Appointment> findAppointmentsByDoctorIdAndPatientNotNull(Long id);
+
+
+
 
 
 }
