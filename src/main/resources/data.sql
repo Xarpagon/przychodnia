98aa49f2557b40d7325ee5753ed3c283d25523f0
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-02-27 09:58:23 +0100

    patientServicetest1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,7 +25,5 @@ Insert into APPOINTMENT (id, doctor_id, patient_id, appointment_Date, appointmen
 Insert into APPOINTMENT (id, doctor_id, patient_id, appointment_Date, appointment_Time, description) values (nextval('appointment_Seq'), '1', '2', '2020-03-04', '10:00', 'hangover');
 
 
-
-
 commit;
 
