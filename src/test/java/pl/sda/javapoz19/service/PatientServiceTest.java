db3dd76634379d4fbcddd6f28904dca0a99feee0
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-02-27 09:40:45 +0100

    patientServicetest

---
+++ src/test/java/pl/sda/javapoz19/service/PatientServiceTest.java
@@ -0,0 +1,32 @@
+package pl.sda.javapoz19.service;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pl.sda.javapoz19.model.Patient;
+
+import java.util.List;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class PatientServiceTest {
+
+    @Autowired
+    private PatientService sut;
+
+    @Test
+    public void showAllPatientsByPeselOrLastName(){
+        //given
+        int expected = 1;
+        //when
+        List<Patient> actual = sut.showAllPatientsByPeselOrLastName("9999999999","Nowek");
+        //then
+        Assert.assertEquals(expected,actual.size());
+    }
+
+
+
+}
