67b06f3a9f08c8b6079d3c45dc4e2677dbe49167
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-02-27 08:57:38 +0100

    patientrepo

--- src/main/java/pl/sda/javapoz19/repository/PatientRepository.java
+++ src/main/java/pl/sda/javapoz19/repository/PatientRepository.java
@@ -3,7 +3,11 @@ package pl.sda.javapoz19.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sda.javapoz19.model.Patient;
 
+import java.util.List;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+    List<Patient> findAllByPeselOrLastName(String pesel, String lastName);
+    List<Patient> deletePatientById(Long id);
 
 }
